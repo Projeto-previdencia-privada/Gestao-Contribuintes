24fb58f35fa03b805a2f4c8f5c4f4a52295988ef
Keven <lkeven77@gmail.com>
2024-04-03 11:41:18 -0300

    gestao contribuintes

--- src/main/java/br/com/gestao_contribuintes/gestaocontribuintes/Controller/ContribuintesController.java
+++ src/main/java/br/com/gestao_contribuintes/gestaocontribuintes/Controller/ContribuintesController.java
@@ -1,6 +1,5 @@
 package br.com.gestao_contribuintes.gestaocontribuintes.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
